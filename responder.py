300c0d7ab00aba263eb145b7e3f232707edef908
hfa033 <hfa033@uit.no>
2024-10-17 09:36:20 +0100

    deleted agents outside src, fixed env

--- responder.py
+++
@@ -1,9 +0,0 @@
-
-from spade.agent import Agent
-from spade.behaviour import CyclicBehaviour
-from spade.message import Message
-from spade import wait_until_finished
-
-class ResponderAgent(Agent):
-    pass
-
